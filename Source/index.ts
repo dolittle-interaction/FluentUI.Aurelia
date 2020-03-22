9591214a1b1d0cb4848ad23174a11ca4140b2517
Einar Ingebrigtsen <einar@dolittle.com>
2020-03-22 11:05:59 +0100

    Moving global resource setup

--- Source/index.ts
+++ Source/index.ts
@@ -1,6 +1,12 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-export function configure(aurelia: any, config: any) {
+import { FrameworkConfiguration } from 'aurelia-framework';
+import { PLATFORM } from 'aurelia-pal';
 
+export function configure(aurelia: FrameworkConfiguration, config: any) {
+    aurelia.globalResources([
+        PLATFORM.moduleName('./components/default-button'),
+        PLATFORM.moduleName('./components/primary-button')
+    ]);
 }
