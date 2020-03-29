442f6394999a273fe0a105d9e11fa06690a93834
Einar Ingebrigtsen <einar@dolittle.com>
2020-03-29 15:54:58 +0200

    Started on support for child items

--- Source/ReactComponent.ts
+++ Source/ReactComponent.ts
@@ -4,18 +4,21 @@
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
 
-import { inlineView, View, ViewResources } from 'aurelia-framework';
+import { inlineView, View, ViewResources, bindable, camelCase } from 'aurelia-framework';
 
 import { ComponentProperties } from './ComponentProperties';
 import { ComponentState } from './ComponentState';
 
 import { ReactStateWrapper } from './ReactStateWrapper';
-
-type Constructor<T extends {} = {}> = new (...args: any[]) => T;
+import { Constructor } from './Constructor';
+import { uniqueIdentifier } from './uniqueIdentifier';
+import { ItemSelector } from './ItemSelector';
+import { parseValue } from './parseValue';
 
 @inlineView('<template><span id.bind="uniqueIdentifier"></span><slot></slot></template>')
 export abstract class ReactComponent<T extends React.Component<TProps, any> | React.FunctionComponent<TProps>, TProps> {
     private _actualComponent: ReactStateWrapper | undefined;
+    private _itemSelectors: ItemSelector[] = [];
 
     static properties<TProps>(properties: TProps) {
         ComponentProperties.configureFor(this, properties);
@@ -23,12 +26,16 @@ export abstract class ReactComponent<T extends React.Component<TProps, any> | Re
 
     uniqueIdentifier: string;
 
+    @bindable
+    visible: boolean = true;
+
     constructor(private _element: Element, private _type: Constructor<T>) {
         if (typeof _type === 'object') {
             this._type = (_type as any).constructor;
         }
 
-        this.uniqueIdentifier = 'au-' + Math.round(Math.random() * 10000000000000000);
+        this.uniqueIdentifier = uniqueIdentifier();
+        this._itemSelectors = this.getItemSelectors();
     }
 
     created(owningView: View, view: View) {
@@ -55,10 +62,35 @@ export abstract class ReactComponent<T extends React.Component<TProps, any> | Re
         const state: any = {};
         state[property] = newValue;
         (this as any)[property] = newValue;
+        this.handleVisibilityProperty(state);
         this._actualComponent?.setState(state);
     }
 
     beforeRender(properties: TProps) {
+    }
+
+    getItemSelectors(): ItemSelector[] {
+        return [];
+    }
+
+    addItem(itemViewModel: any, item: any) {
+        const filtered = this._itemSelectors.filter(_ => _.type === itemViewModel.constructor);
+        if (filtered.length === 1) {
+            const itemSelector = filtered[0];
+
+            const thisAsAny = this as any;
+
+            if (!thisAsAny[itemSelector.targetProperty]) {
+                thisAsAny[itemSelector.targetProperty] = [];
+            }
+            if (Object.prototype.toString.call(thisAsAny[itemSelector.targetProperty]) !== '[object Array]') {
+                throw new Error(`Property '${itemSelector.targetProperty}' on '${itemSelector.type}' is not an array. Can't add items.`);
+            }
+
+            thisAsAny[itemSelector.targetProperty].push(item);
+
+            this.propertyChanged(itemSelector.targetProperty, thisAsAny[itemSelector.targetProperty]);
+        }
     }
 
     attached() {
@@ -70,7 +102,16 @@ export abstract class ReactComponent<T extends React.Component<TProps, any> | Re
 
         this.beforeRender(properties);
 
+        this.handleVisibilityProperty(properties);
+
         const reactElement = React.createElement(ReactStateWrapper, properties);
         this._actualComponent = ReactDom.render(reactElement, container);
+    }
+
+    private handleVisibilityProperty(properties: any) {
+        if (properties.hasOwnProperty('visible')) {
+            delete properties.visible;
+        }
+        properties.hidden = !this.visible;
     }
 }
