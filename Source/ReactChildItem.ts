7b2bb14b1c997d3fbb665335a4c6be072e1cc5eb
Einar Ingebrigtsen <einar@dolittle.com>
2020-03-31 19:05:18 +0200

    Renaming to ReactChildItem

--- Source/ReactChildItem.ts
+++ Source/ReactChildItem.ts
@@ -1,26 +1,21 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import { ComponentProperties } from './ComponentProperties';
-import { uniqueIdentifier } from './uniqueIdentifier';
 import { ComponentState } from './ComponentState';
 import { IComponent } from './IComponent';
+import { ReactBase } from './ReactBase';
+import { Constructor } from './Constructor';
 
-export abstract class ReactItem<TItem> {
-    static properties<TProps>(properties: TProps) {
-        ComponentProperties.configureFor(this, properties);
-    }
-
-    uniqueIdentifier: string;
-
+export class ReactChildItem<TItem> extends ReactBase {
     constructor(private _element: Element) {
-        this.uniqueIdentifier = uniqueIdentifier();
+        super();
     }
 
     propertyChanged(property: string, newValue: any) {
     }
 
     attached() {
+        this.handlePropertyConverters();
         const viewModel = (this._element.parentElement as any)?.au?.controller?.viewModel as IComponent;
         if (viewModel) {
             const properties = ComponentState.createFor(this);
