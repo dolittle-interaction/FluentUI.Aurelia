d8a2f49d6421174781a594586dab30c4fcb57a8a
Einar Ingebrigtsen <einar@dolittle.com>
2020-04-04 08:51:44 +0200

    Changing signature to require component

--- Source/ComponentState.ts
+++ Source/ComponentState.ts
@@ -3,14 +3,15 @@
 
 import { ComponentProperties } from './ComponentProperties';
 import { parseValue } from './parseValue';
+import { IComponent } from './IComponent';
 
 export class ComponentState {
 
-    static updateFor(target: any, state: any, element: Element, child: boolean = false): void {
+    static updateFor(target: IComponent, state: any, element: Element, child: boolean = false): void {
         const properties = ComponentProperties.getFor(target.constructor);
 
         properties.forEach(property => {
-            const targetValue = target[property.name];
+            const targetValue = (target as any)[property.name];
             if (property.isEvent && child) {
                 state[property.reactName] = (args: any) => element.dispatchEvent(new CustomEvent(property.name, { bubbles: true, detail: args }));
             } else if (property.isFunction) {
