05aa30eaa692bf843cbaa7adfc02060ef44d79a5
Einar Ingebrigtsen <einar@dolittle.com>
2020-04-05 12:44:12 +0200

    Introducing ReactItemsComponent

---
+++ Source/React/ReactItemsComponent.ts
@@ -0,0 +1,22 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import * as React from 'react';
+
+export class ReactItemsComponent extends React.Component {
+    private _type: any;
+
+    constructor(properties: any) {
+        super(properties);
+
+        this._type = properties._componentType;
+        this.state = {...properties};
+    }
+
+    render() {
+        return React.createElement(
+            this._type,
+            this.state
+        );
+    }
+}
