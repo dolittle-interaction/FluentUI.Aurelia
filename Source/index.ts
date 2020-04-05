6bc571abdf8f8e7d13af25c65a2b46ef538ceaf3
Einar Ingebrigtsen <einar@dolittle.com>
2020-04-05 18:59:26 +0200

    Adding ComboBox

--- Source/index.ts
+++ Source/index.ts
@@ -38,6 +38,8 @@ export function configure(aurelia: FrameworkConfiguration, config: any) {
         PLATFORM.moduleName('./components/basic/command-bar-button'),
         PLATFORM.moduleName('./components/basic/action-button'),
         PLATFORM.moduleName('./components/basic/checkbox'),
+        PLATFORM.moduleName('./components/basic/combo-box'),
+        PLATFORM.moduleName('./components/basic/combo-box-option'),
         PLATFORM.moduleName('./components/basic/toggle'),
         PLATFORM.moduleName('./components/basic/choice-group'),
         PLATFORM.moduleName('./components/basic/choice-group-option'),
