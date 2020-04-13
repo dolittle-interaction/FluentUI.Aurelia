9c87958c990e50b4e4dd1e0e17b74b5ff6cb03da
Einar Ingebrigtsen <einar@dolittle.com>
2020-04-13 14:45:38 +0200

    Making base button an items component
    Also moving down everything related to
    context menu item support

--- Source/components/basic/ButtonBase.ts
+++ Source/components/basic/ButtonBase.ts
@@ -3,18 +3,41 @@
 
 import { bindable } from 'aurelia-framework';
 
+import { IButtonProps, IContextualMenuProps } from 'office-ui-fabric-react';
 
-import { IButtonProps } from 'office-ui-fabric-react';
+import {
+    CallbackItemHandlingStrategy,
+    IItemsComponent,
+    IItemHandlingStrategy,
+    ItemsComponent,
+    PropertyConverter,
+    UIElement,
+    IconTypeConverter
+} from '../../index';
 
-import { ContentComponent, PropertyConverter, IconTypeConverter } from '../../index';
+import { ContextualMenuItem } from '../commands/contextual-menu-item';
 
-export class ButtonBase<TComponent extends React.Component<IButtonProps, any>> extends ContentComponent<TComponent, IButtonProps> implements IButtonProps {
-    hidden: boolean = false;
-
+export class ButtonBase<TComponent extends React.Component<IButtonProps, any>> extends ItemsComponent<IButtonProps, TComponent> {
     @bindable
     icon: string = '';
 
+    getItemHandlingStrategies(): IItemHandlingStrategy[] {
+        return [new CallbackItemHandlingStrategy(ContextualMenuItem, this.handleContextualMenuItem)];
+    }
+
     getPropertyConverters(): PropertyConverter[] {
         return [new PropertyConverter('icon', 'iconProps', new IconTypeConverter())];
+    }
+
+    private handleContextualMenuItem(target: IItemsComponent, item: UIElement) {
+        const buttonProps = this as IButtonProps;
+        if (!buttonProps.menuProps) {
+            buttonProps.menuProps = {
+                items: []
+            } as IContextualMenuProps;
+        }
+
+        buttonProps.menuProps.items = [...buttonProps.menuProps.items, item.state];
+        target.propertyChanged('menuProps', buttonProps.menuProps);
     }
 }
--- Source/components/basic/command-bar-button.ts
+++ Source/components/basic/command-bar-button.ts
@@ -1,54 +1,18 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import { customElement, autoinject, bindable } from 'aurelia-framework';
+import { customElement, autoinject } from 'aurelia-framework';
 
-import { IButtonProps, CommandBarButton, IContextualMenuProps } from 'office-ui-fabric-react';
+import { IButtonProps, CommandBarButton } from 'office-ui-fabric-react';
 
-import { ContextualMenuItem } from '../commands/contextual-menu-item';
-
-import {
-    CallbackItemHandlingStrategy,
-    IItemsComponent,
-    IItemHandlingStrategy,
-    ItemsComponent,
-    PropertyConverter,
-    UIElement,
-    IconTypeConverter
-} from '../../index';
-
+import { ButtonBase } from './ButtonBase';
 import ButtonProps from './ButtonProps';
 
 @autoinject
 @customElement('command-bar-button')
-export class AuCommandBarButton extends ItemsComponent<CommandBarButton> {
-    hidden: boolean = false;
-
-    @bindable
-    icon: string = '';
-
+export class AuCommandBarButton extends ButtonBase<CommandBarButton> {
     constructor(element: Element) {
         super(element, CommandBarButton);
-    }
-
-    getItemHandlingStrategies(): IItemHandlingStrategy[] {
-        return [new CallbackItemHandlingStrategy(ContextualMenuItem, this.handleContextualMenuItem)];
-    }
-
-    getPropertyConverters(): PropertyConverter[] {
-        return [new PropertyConverter('icon', 'iconProps', new IconTypeConverter())];
-    }
-
-    private handleContextualMenuItem(target: IItemsComponent, item: UIElement) {
-        const buttonProps = this as IButtonProps;
-        if (!buttonProps.menuProps) {
-            buttonProps.menuProps = {
-                items: []
-            } as IContextualMenuProps;
-        }
-
-        buttonProps.menuProps.items = [...buttonProps.menuProps.items, item.state];
-        target.propertyChanged('menuProps', buttonProps.menuProps);
     }
 }
 
