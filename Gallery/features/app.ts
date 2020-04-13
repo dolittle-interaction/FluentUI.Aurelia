aa35384c6333b6ca517c5b6d99dd3e30dc00cd22
Einar Ingebrigtsen <einar@dolittle.com>
2020-04-13 19:39:25 +0200

    Reorganizing navigation and adding searching

--- Gallery/features/app.ts
+++ Gallery/features/app.ts
@@ -5,6 +5,8 @@ import { autoinject } from 'aurelia-dependency-injection';
 import { PLATFORM } from 'aurelia-pal';
 import { Router, RouterConfiguration } from 'aurelia-router';
 
+import * as navigation from './navigation';
+
 @autoinject
 export class App {
     router: any;
@@ -13,25 +15,9 @@ export class App {
         config.options.pushState = true;
         config.map([
             { route: ['', '/'], name: 'Index', moduleId: PLATFORM.moduleName('index'), nav: true },
-
-            // Basic
-            { route: '/components/basic/button', name: 'Button', moduleId: PLATFORM.moduleName('./components/basic/button/index'), nav: true },
-            { route: '/components/basic/checkbox', name: 'Checkbox', moduleId: PLATFORM.moduleName('./components/basic/checkbox/index'), nav: true },
-            { route: '/components/basic/choice-group', name: 'ChoiceGroup', moduleId: PLATFORM.moduleName('./components/basic/choice-group/index'), nav: true },
-            { route: '/components/basic/combo-box', name: 'ComboBox', moduleId: PLATFORM.moduleName('./components/basic/combo-box/index'), nav: true },
-            { route: '/components/basic/dropdown', name: 'Dropdown', moduleId: PLATFORM.moduleName('./components/basic/dropdown/index'), nav: true },
-            { route: '/components/basic/label', name: 'Label', moduleId: PLATFORM.moduleName('./components/basic/label/index'), nav: true },
-            { route: '/components/basic/link', name: 'Link', moduleId: PLATFORM.moduleName('./components/basic/link/index'), nav: true },
-            { route: '/components/basic/rating', name: 'Rating', moduleId: PLATFORM.moduleName('./components/basic/rating/index'), nav: true },
-            { route: '/components/basic/search-box', name: 'SearchBox', moduleId: PLATFORM.moduleName('./components/basic/search-box/index'), nav: true },
-            { route: '/components/basic/slider', name: 'Slider', moduleId: PLATFORM.moduleName('./components/basic/slider/index'), nav: true },
-
-            // Commands
-            { route: '/components/commands/pivot', name: 'Pivot', moduleId: PLATFORM.moduleName('./components/commands/pivot/index'), nav: true },
-
-            // Utilities
-            { route: '/components/utilities/stack', name: 'stack', moduleId: PLATFORM.moduleName('./components/utilities/stack/index'), nav: true }
         ]);
+
+        config.map(navigation.default);
 
         this.router = router;
     }
---
+++ Gallery/features/navigation.ts
@@ -0,0 +1,17 @@
+/* GENERATED CODE */
+import { PLATFORM } from 'aurelia-pal';
+
+export default [
+   { route: '/components/basic/button', name: 'Button', moduleId: PLATFORM.moduleName('./components/basic/button/index'), nav: true },
+   { route: '/components/basic/checkbox', name: 'Checkbox', moduleId: PLATFORM.moduleName('./components/basic/checkbox/index'), nav: true },
+   { route: '/components/basic/choice-group', name: 'ChoiceGroup', moduleId: PLATFORM.moduleName('./components/basic/choice-group/index'), nav: true },
+   { route: '/components/basic/combo-box', name: 'ComboBox', moduleId: PLATFORM.moduleName('./components/basic/combo-box/index'), nav: true },
+   { route: '/components/basic/dropdown', name: 'Dropdown', moduleId: PLATFORM.moduleName('./components/basic/dropdown/index'), nav: true },
+   { route: '/components/basic/label', name: 'Label', moduleId: PLATFORM.moduleName('./components/basic/label/index'), nav: true },
+   { route: '/components/basic/link', name: 'Link', moduleId: PLATFORM.moduleName('./components/basic/link/index'), nav: true },
+   { route: '/components/basic/rating', name: 'Rating', moduleId: PLATFORM.moduleName('./components/basic/rating/index'), nav: true },
+   { route: '/components/basic/search-box', name: 'SearchBox', moduleId: PLATFORM.moduleName('./components/basic/search-box/index'), nav: true },
+   { route: '/components/basic/slider', name: 'Slider', moduleId: PLATFORM.moduleName('./components/basic/slider/index'), nav: true },
+   { route: '/components/commands/pivot', name: 'Pivot', moduleId: PLATFORM.moduleName('./components/commands/pivot/index'), nav: true },
+   { route: '/components/utilities/stack', name: 'Stack', moduleId: PLATFORM.moduleName('./components/utilities/stack/index'), nav: true },
+];
