98c9a63d525048a0b8a76f125a7392040b5f4ceb
Einar Ingebrigtsen <einar@dolittle.com>
2020-06-01 08:05:01 +0200

    Cleanup

--- Source/components/basic/link.ts
+++ Source/components/basic/link.ts
@@ -2,7 +2,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 import * as React from 'react';
-import * as ReactDom from 'react-dom';
 
 import { customElement, autoinject } from 'aurelia-framework';
 
